62009a944470830817089961b7fa0c91f55c3d3d
Nicolas Seine <nicolas.seine@gmail.com>
2022-01-10 17:22:58 +0100

    Works without bugs

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ class Game
   def load_dictionary(file)
     dict = File.open(file, 'r')
     list_of_words = []
-    dict.each {|line| list_of_words << line.chomp.downcase if line.chomp.length.between?(5,12) }
+    dict.each {|line| list_of_words << line.upcase.chomp if line.chomp.length.between?(5,12) }
     list_of_words
   end
 
@@ -21,16 +21,7 @@ class Game
 
   def new_turn
     display_state_of_the_game
-    puts "Enter your guess: "
-    guess = ''
-    loop do
-      guess = gets.chomp
-      break if guess.match(/[a-zA-Z]/)
-      
-      guess = guess[0].downcase
-      puts guess
-    end
-    
+    guess = correct_input
     if @word_to_guess.include?(guess) 
       @right_guesses << guess 
     else 
@@ -42,8 +33,17 @@ class Game
 
   def display_state_of_the_game
     puts "You have #{@guesses_left} guesses left \n"
-    puts "Wrong guesses : #{@wrong_guesses.join(', ')}" unless @guesses_left == 10
+    @guesses_left == 10 || puts("Wrong guesses : #{@wrong_guesses.join(', ')}")
     puts "Find the word : #{display_word(@word_to_guess)}"
+  end
+
+  def correct_input
+    char = ''
+    until (char.length == 1 && char.match(/[a-zA-Z]/)) do 
+      puts "Enter your guess: "
+      char = gets.chomp
+    end
+    char.upcase
   end
 
   def display_word(original)
@@ -55,13 +55,26 @@ class Game
     if @guesses_left == 0
       puts "No guesses left... You lost"
       puts "The word to guess was #{@word_to_guess.join('')}"
+      ask_for_a_new_game
     elsif display_word(@word_to_guess).include?("_")
       new_turn
     else
       puts "Victory ! Congratulations!"
       puts "The word to find was indeed #{@word_to_guess.join('')}"
+      ask_for_a_new_game
     end
   end
+
+  def ask_for_a_new_game
+    puts "Do you want to play again ? (Y/n)"
+    answer = gets.chomp
+    if answer.match(/^[Nn]/)
+      exit
+    else
+      new_round
+    end
+  end
+
 end
 
 
