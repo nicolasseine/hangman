b7a2b9347aea5821ed92d25e5ab4bb65b15f29e1
Nicolas Seine <nicolas.seine@gmail.com>
2022-01-11 15:48:40 +0100

    Working save and load functions

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,19 @@
+require 'yaml'
+
 class Game
-  attr_reader :dict
-  def initialize(file)
+    def initialize(file)
+    @guesses_left = 10
+    @right_guesses = []
+    @wrong_guesses = []
+    @word_to_guess = []
     @dict = load_dictionary(file)
+    start_new_game
   end
 
+  attr_reader :dict
+  attr_accessor :guesses_left, :right_guesses, :wrong_guesses, :word_to_guess
+
+ 
   def load_dictionary(file)
     dict = File.open(file, 'r')
     list_of_words = []
@@ -11,56 +21,61 @@ class Game
     list_of_words
   end
 
-  def new_round
-    @word_to_guess = dict.sample.split('')
-    @guesses_left = 10
-    @right_guesses = []
-    @wrong_guesses = []
+  def start_new_game
+    puts "Do you want to load a previous game ? (y/N)"
+    answer = gets.chomp
+    answer.match(/^[YyOo]/) ? load_game : new_round
     new_turn
+  end
+
+  def new_round
+    self.word_to_guess = dict.sample.split('')
   end
 
   def new_turn
     display_state_of_the_game
     guess = correct_input
-    if @word_to_guess.include?(guess) 
-      @right_guesses << guess 
+    if guess == 'SAVE'
+      save_game
+    elsif word_to_guess.include?(guess) 
+      self.right_guesses << guess 
     else 
-      @wrong_guesses << guess
+      self.wrong_guesses << guess
       @guesses_left -= 1
     end
     check_end_of_game
   end
 
   def display_state_of_the_game
-    puts "You have #{@guesses_left} guesses left \n"
-    @guesses_left == 10 || puts("Wrong guesses : #{@wrong_guesses.join(', ')}")
-    puts "Find the word : #{display_word(@word_to_guess)}"
+    puts "\nYou have #{guesses_left} guesses left \n"
+    guesses_left == 10 || puts("Wrong guesses : #{wrong_guesses.join(', ')}")
+    puts "Find the word : #{display_word(word_to_guess)}"
   end
 
   def correct_input
-    char = ''
-    until (char.length == 1 && char.match(/[a-zA-Z]/)) do 
-      puts "Enter your guess: "
-      char = gets.chomp
+    input = ''
+    until input == "SAVE" || (input.length == 1 && input.match(/[a-zA-Z]/)) do 
+      puts "Enter your guess, or type SAVE to save the game: "
+      input = gets.chomp
     end
-    char.upcase
+    input.upcase
   end
 
   def display_word(original)
-    original.map {|letter| @right_guesses.include?(letter) ? letter : "_"}
+    original.map {|letter| right_guesses.include?(letter) ? letter : "_"}
                  . join(" ")
   end
 
   def check_end_of_game
-    if @guesses_left == 0
+    if guesses_left == 0
       puts "No guesses left... You lost"
-      puts "The word to guess was #{@word_to_guess.join('')}"
+      puts "The word to guess was #{word_to_guess.join('')}"
       ask_for_a_new_game
-    elsif display_word(@word_to_guess).include?("_")
+    elsif display_word(word_to_guess).include?("_")
       new_turn
     else
       puts "Victory ! Congratulations!"
-      puts "The word to find was indeed #{@word_to_guess.join('')}"
+      puts "The word to find was indeed #{word_to_guess.join('')}"
       ask_for_a_new_game
     end
   end
@@ -71,12 +86,37 @@ class Game
     if answer.match(/^[Nn]/)
       exit
     else
-      new_round
+      Game.new("5desk.txt")
     end
+  end
+
+  def state_of_the_game
+    {
+    left: @guesses_left,
+    wrong: @wrong_guesses,
+    right: @right_guesses,
+    word: @word_to_guess,
+    }
+  end
+
+  def save_game
+    
+    yaml = YAML::dump(self.state_of_the_game)
+    File.write("save.yaml", yaml)
+    exit
+  end
+
+  def load_game
+    file = File.open('save.yaml', 'r')
+    data = YAML.load file.read
+    self.guesses_left = data[:left]
+    self.wrong_guesses = data[:wrong]
+    self.right_guesses = data[:right]
+    self.word_to_guess = data[:word]
   end
 
 end
 
 
 
-Game.new("5desk.txt").new_round
+Game.new("5desk.txt")
